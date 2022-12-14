2285f9143ef100b0673c772fc8f3c8403ce203de
SILVESTRI_GREGORY <gregory.silvestri45@gmail.com>
2022-12-14 22:08:50 +0100

    visibility/const

---
+++ src/DataFixtures/LessonFixtures.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\Lesson;
+use Doctrine\Persistence\ObjectManager;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Faker\Factory;
+
+class LessonFixtures extends Fixture
+{
+   public const TITLES = [
+        'Utiliser Blue Line',
+        'Allumer son téléphone',
+        'Gérer sa batterie',
+    ];
+
+    public function load(ObjectManager $manager): void
+    {
+        $faker = Factory::create();
+
+        foreach (self::TITLES as $lessonName) {
+            $lesson = new Lesson();
+            $lesson->setTitle($lessonName);
+            $lesson->setDescription(($faker->paragraphs(1, true)));
+            $manager->persist($lesson);
+        }
+        $manager->flush();
+    }
+}
---
+++ src/Entity/Lesson.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\LessonRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: LessonRepository::class)]
+class Lesson
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $title = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $description = null;
+
+    #[ORM\Column(length: 255, nullable: true)]
+    private ?string $video = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getTitle(): ?string
+    {
+        return $this->title;
+    }
+
+    public function setTitle(string $title): self
+    {
+        $this->title = $title;
+
+        return $this;
+    }
+
+    public function getDescription(): ?string
+    {
+        return $this->description;
+    }
+
+    public function setDescription(string $description): self
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+
+    public function getVideo(): ?string
+    {
+        return $this->video;
+    }
+
+    public function setVideo(string $video): self
+    {
+        $this->video = $video;
+
+        return $this;
+    }
+}
