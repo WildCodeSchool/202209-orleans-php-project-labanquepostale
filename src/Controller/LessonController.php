7d3e2f72a6ce7ecd379cda8c0627b8943fc52e4f
DRISS BOUMEDIENE <driss308@hotmail.fr>
2022-12-15 14:26:03 +0100

    lala

--- src/Controller/LessonController.php
+++ src/Controller/LessonController.php
@@ -4,6 +4,7 @@ namespace App\Controller;
 
 use App\Entity\Lesson;
 use App\Repository\LessonRepository;
+use App\Repository\QuestionRepository;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -21,10 +22,13 @@ class LessonController extends AbstractController
         ]);
     }
     #[Route('/show/{id}', requirements: ['id' => '\d+'], name: 'show')]
-    public function show(Lesson $lesson): Response
+    public function show(Lesson $lesson, QuestionRepository $questionRepository): Response
     {
+        $questions = $questionRepository->findAll();
+
         return $this->render('lesson/show.html.twig', [
             'lesson' => $lesson,
+            'questions' => $questions
         ]);
     }
 }
--- src/DataFixtures/QuestionFixtures.php
+++ src/DataFixtures/QuestionFixtures.php
@@ -2,16 +2,26 @@
 
 namespace App\DataFixtures;
 
-use Doctrine\Bundle\FixturesBundle\Fixture;
+use App\Entity\Question;
 use Doctrine\Persistence\ObjectManager;
+use Doctrine\Bundle\FixturesBundle\Fixture;
 
 class QuestionFixtures extends Fixture
 {
+    public const TESTS =
+    [
+        'Je peux allumer mon téléphone s\'il charge :',
+        'Augmenter la luminosité consomme plus de batterie :',
+        'Je donne mon numéro de sécurité sociale si on me le demande par mail :'
+    ];
+
     public function load(ObjectManager $manager): void
     {
-        // $product = new Product();
-        // $manager->persist($product);
-
+        foreach (self::TESTS as $test) {
+            $question = new Question();
+            $question->setTest($test);
+            $manager->persist($question);
+        }
         $manager->flush();
     }
 }
