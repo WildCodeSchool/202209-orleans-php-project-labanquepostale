05ee2b501e9125f428ef181ea5b26a76a2b6a4dc
DRISS BOUMEDIENE <driss308@hotmail.fr>
2022-12-15 12:52:24 +0100

    created entity

---
+++ src/Entity/Question.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\QuestionRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: QuestionRepository::class)]
+class Question
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $test = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getTest(): ?string
+    {
+        return $this->test;
+    }
+
+    public function setTest(string $test): self
+    {
+        $this->test = $test;
+
+        return $this;
+    }
+}
