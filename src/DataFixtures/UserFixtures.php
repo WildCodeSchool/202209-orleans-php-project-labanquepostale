a975b6a570892b0ad3b31c7cbdb0eae7f4f05b27
teddyandria <teddy.andriamam@gmail.com>
2023-01-03 16:20:01 +0100

    login

---
+++ src/DataFixtures/UserFixtures.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\User;
+use Doctrine\Persistence\ObjectManager;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+
+class UserFixtures extends Fixture
+{
+
+    public function __construct(private UserPasswordHasherInterface $passwordHasher)
+    {
+    }
+    public function load(ObjectManager $manager): void
+    {
+        $user = new User();
+        $user->setEmail('user@email.com');
+        $user->setFirstName('Teddy');
+        $user->setLastName('Slexiqe');
+        $hashedPassword = $this->passwordHasher->hashPassword(
+            $user,
+            'azerty'
+        );
+        $user->setPassword($hashedPassword);
+        $manager->persist($user);
+
+        $admin = new User();
+        $admin->setEmail('admin@email.com');
+        $admin->setFirstName('Jacky');
+        $admin->setLastName('Chan');
+        $admin->setRoles(['ROLE_ADMIN']);
+        $hashedPassword = $this->passwordHasher->hashPassword(
+            $admin,
+            'admin1234'
+        );
+        $admin->setPassword($hashedPassword);
+        $manager->persist($admin);
+
+        $manager->flush();
+    }
+}
