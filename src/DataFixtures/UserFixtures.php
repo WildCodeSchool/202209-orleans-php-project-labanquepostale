ef2a9c24309e859fcf300d0f81d3ae157cf314b6
teddyandria <teddy.andriamam@gmail.com>
2023-01-03 16:20:51 +0100

    login fix

--- src/DataFixtures/UserFixtures.php
+++ src/DataFixtures/UserFixtures.php
@@ -9,7 +9,6 @@ use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 
 class UserFixtures extends Fixture
 {
-
     public function __construct(private UserPasswordHasherInterface $passwordHasher)
     {
     }
