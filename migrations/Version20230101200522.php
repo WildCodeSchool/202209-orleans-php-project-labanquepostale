b40ab7c8a15eabae3749323fee8d1f4b51141cb7
teddyandria <teddy.andriamam@gmail.com>
2023-01-04 16:41:40 +0100

    delete 1 migration file

--- migrations/Version20230101200522.php
+++ migrations/Version20230101200522.php
@@ -21,11 +21,13 @@ final class Version20230101200522 extends AbstractMigration
     {
         // this up() migration is auto-generated, please modify it to your needs
         $this->addSql('CREATE TABLE user (id INT AUTO_INCREMENT NOT NULL, email VARCHAR(180) NOT NULL, roles JSON NOT NULL, password VARCHAR(255) NOT NULL, UNIQUE INDEX UNIQ_8D93D649E7927C74 (email), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('ALTER TABLE user ADD first_name VARCHAR(255) NOT NULL, ADD last_name VARCHAR(255) NOT NULL');
     }
 
     public function down(Schema $schema): void
     {
         // this down() migration is auto-generated, please modify it to your needs
         $this->addSql('DROP TABLE user');
+        $this->addSql('ALTER TABLE user DROP first_name, DROP last_name');
     }
 }
