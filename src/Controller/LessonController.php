7f79fe56d8f27713c1851804cab72bacd04fcccb
SILVESTRI_GREGORY <gregory.silvestri45@gmail.com>
2023-01-16 09:16:35 +0100

    validation

--- src/Controller/LessonController.php
+++ src/Controller/LessonController.php
@@ -12,6 +12,7 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Service\ValidateAnswer;
 
 #[Route('/lesson', name: 'lesson_')]
 class LessonController extends AbstractController
---
+++ src/Controller/Service/ValidateAnswer.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Service;
+
+use App\Entity\Response;
+use App\Repository\ResponseRepository;
+
+class ValidateAnswer
+{
+    /* private $miniLvlExpected =
+    private int $nbrLesson;
+
+    private ResponseRepository $responseRepository;
+
+    public function __construct(ResponseRepository $responseRepository)
+    {
+        $this->responseRepository = $responseRepository;
+    }
+   public function isvalidated(array $answerId): bool
+   {
+    foreach( $answerId as $answer){
+       return $answer->isCorrect();
+
+    }
+   } */
+}
