e8202ef48c49ade7cd7dc6b67c499a5433fdbd7f
Violeta Doncheva <violeta.doncheva90@gmail.com>
2023-01-18 14:44:59 +0100

    Updates done on Tutorial

--- src/Form/TutorialType.php
+++ src/Form/TutorialType.php
@@ -6,14 +6,16 @@ use App\Entity\Tutorial;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 
 class TutorialType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('title')
-        ;
+            ->add('title', TextType::class, [
+            'label' => 'Titre',
+        ]);
     }
 
     public function configureOptions(OptionsResolver $resolver): void
