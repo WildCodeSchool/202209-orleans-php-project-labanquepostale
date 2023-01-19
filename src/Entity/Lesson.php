0192c9bc189c079f4151e162f51cccbe1caeb74a
DRISS BOUMEDIENE <driss308@hotmail.fr>
2023-01-19 16:40:48 +0100

    lesson creation done

--- src/Entity/Lesson.php
+++ src/Entity/Lesson.php
@@ -6,6 +6,7 @@ use App\Repository\LessonRepository;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
 
 #[ORM\Entity(repositoryClass: LessonRepository::class)]
 class Lesson
@@ -16,12 +17,17 @@ class Lesson
     private ?int $id = null;
 
     #[ORM\Column(length: 255)]
+    #[Assert\NotBlank]
+    #[Assert\Length(max: 255)]
     private ?string $title = null;
 
     #[ORM\Column(length: 255)]
+    #[Assert\NotBlank]
+    #[Assert\Length(max: 255)]
     private ?string $description = null;
 
     #[ORM\Column(length: 255, nullable: true)]
+    #[Assert\NotBlank]
     private ?string $video = null;
 
     #[ORM\OneToMany(mappedBy: 'lesson', targetEntity: Question::class)]
