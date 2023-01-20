76294cd09413a4bfaf6a930e16eb6f79a2ee4822
DRISS BOUMEDIENE <driss308@hotmail.fr>
2023-01-20 09:42:33 +0100

    corrected mapping

--- src/Form/Lesson1Type.php
+++ src/Form/Lesson1Type.php
@@ -28,7 +28,7 @@ class Lesson1Type extends AbstractType
                 'choice_label' => 'title',
                 'multiple' => false,
                 'expanded' => true,
-                'mapped' => false,
+                'mapped' => true,
                 'constraints' => [
                     new NotBlank([
                         'message' => 'Choisissez un tutoriel'
