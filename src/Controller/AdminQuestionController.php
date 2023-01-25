110c6453282c440a8c270cd17fe869d9b767aca2
DRISS BOUMEDIENE <driss308@hotmail.fr>
2023-01-25 09:57:12 +0100

    cleaned controllers

--- src/Controller/AdminQuestionController.php
+++ src/Controller/AdminQuestionController.php
@@ -2,13 +2,15 @@
 
 namespace App\Controller;
 
+use App\Entity\Lesson;
 use App\Entity\Question;
+use App\Entity\Tutorial;
 use App\Form\QuestionType;
 use App\Repository\QuestionRepository;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 #[Route('/admin/question')]
 class AdminQuestionController extends AbstractController
@@ -40,11 +42,30 @@ class AdminQuestionController extends AbstractController
         ]);
     }
 
-    #[Route('/{id}', name: 'app_admin_question_show', methods: ['GET'])]
-    public function show(Question $question): Response
-    {
-        return $this->render('admin_question/show.html.twig', [
-            'question' => $question,
+    #[Route('/{tutorial}/leçons/{lesson}/quiz', name: 'app_admin_tutorial_lesson_quiz', methods: ['GET'])]
+    public function showQuiz(
+        Request $request,
+        Tutorial $tutorial,
+        Lesson $lesson,
+        Question $question,
+        QuestionRepository $questionRepository
+    ): Response {
+        $questions = $lesson->getQuestions();
+
+        $form = $this->createForm(QuestionType::class, $question);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $questionRepository->save($question, true);
+
+            return $this->redirectToRoute('app_admin_tutorial_lesson_quiz', [], Response::HTTP_SEE_OTHER);
+        }
+
+        return $this->renderForm('admin_tutorial/quiz_index.html.twig', [
+            'tutorial' => $tutorial,
+            'lesson' => $lesson,
+            'questions' => $questions,
+            'form' => $form
         ]);
     }
 
--- src/Controller/AdminTutorialController.php
+++ src/Controller/AdminTutorialController.php
@@ -2,22 +2,18 @@
 
 namespace App\Controller;
 
-use App\Entity\Lesson;
-use App\Entity\Question;
 use App\Entity\Tutorial;
-use App\Form\QuestionType;
 use App\Form\TutorialType;
-use App\Repository\QuestionRepository;
 use App\Repository\TutorialRepository;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
-#[Route('/admin/tutoriel')]
+#[Route('/admin/tutoriel', name: 'app_admin_tutorial_')]
 class AdminTutorialController extends AbstractController
 {
-    #[Route('/', name: 'app_admin_tutorial_index', methods: ['GET'])]
+    #[Route('/', name: 'index', methods: ['GET'])]
     public function index(TutorialRepository $tutorialRepository): Response
     {
         return $this->render('admin_tutorial/index.html.twig', [
@@ -25,8 +21,8 @@ class AdminTutorialController extends AbstractController
         ]);
     }
 
-    #[Route('/ajouter', name: 'app_admin_tutorial_new', methods: ['GET', 'POST'])]
-    public function new(Request $request, TutorialRepository $tutorialRepository): Response
+    #[Route('/ajouter', name: 'new', methods: ['GET', 'POST'])]
+    public function newTutorial(Request $request, TutorialRepository $tutorialRepository): Response
     {
         $tutorial = new Tutorial();
         $form = $this->createForm(TutorialType::class, $tutorial);
@@ -45,35 +41,8 @@ class AdminTutorialController extends AbstractController
         ]);
     }
 
-    #[Route('/{tutorial}/leçons/{lesson}/quiz', name: 'app_admin_tutorial_lesson_quiz', methods: ['GET'])]
-    public function showQuiz(
-        Request $request,
-        Tutorial $tutorial,
-        Lesson $lesson,
-        Question $question,
-        QuestionRepository $questionRepository
-    ): Response {
-        $questions = $lesson->getQuestions();
-
-        $form = $this->createForm(QuestionType::class, $question);
-        $form->handleRequest($request);
-
-        if ($form->isSubmitted() && $form->isValid()) {
-            $questionRepository->save($question, true);
-
-            return $this->redirectToRoute('app_admin_tutorial_lesson_quiz', [], Response::HTTP_SEE_OTHER);
-        }
-
-        return $this->renderForm('admin_tutorial/quiz_index.html.twig', [
-            'tutorial' => $tutorial,
-            'lesson' => $lesson,
-            'questions' => $questions,
-            'form' => $form
-        ]);
-    }
-
-    #[Route('/{id}/editer', name: 'app_admin_tutorial_edit', methods: ['GET', 'POST'])]
-    public function edit(Request $request, Tutorial $tutorial, TutorialRepository $tutorialRepository): Response
+    #[Route('/{id}/editer', name: 'edit', methods: ['GET', 'POST'])]
+    public function editTutorial(Request $request, Tutorial $tutorial, TutorialRepository $tutorialRepository): Response
     {
         $form = $this->createForm(TutorialType::class, $tutorial);
         $form->handleRequest($request);
@@ -91,11 +60,15 @@ class AdminTutorialController extends AbstractController
         ]);
     }
 
-    #[Route('/{id}', name: 'app_admin_tutorial_delete', methods: ['POST'])]
-    public function delete(Request $request, Tutorial $tutorial, TutorialRepository $tutorialRepository): Response
-    {
+    #[Route('/{id}', name: 'delete', methods: ['POST'])]
+    public function deleteTutorial(
+        Request $request,
+        Tutorial $tutorial,
+        TutorialRepository $tutorialRepository
+    ): Response {
         if ($this->isCsrfTokenValid('delete' . $tutorial->getId(), $request->request->get('_token'))) {
             $tutorialRepository->remove($tutorial, true);
+            $this->addFlash('danger', 'Le nouveau tutoriel a été supprimé avec succès.');
         }
 
         return $this->redirectToRoute('app_admin_tutorial_index', [], Response::HTTP_SEE_OTHER);
