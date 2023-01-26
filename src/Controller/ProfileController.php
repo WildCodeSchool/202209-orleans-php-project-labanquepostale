331a15260b12b4692ab5f1825027f26bc08085ab
Violeta Doncheva <violeta.doncheva90@gmail.com>
2023-01-26 14:27:40 +0100

    add_e_on_profile_page

---
+++ src/Controller/ProfileController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+class ProfileController extends AbstractController
+{
+    #[Route('/profile', name: 'app_profile')]
+    public function index(): Response
+    {
+        return $this->render('profile/profile.html.twig');
+    }
+}
