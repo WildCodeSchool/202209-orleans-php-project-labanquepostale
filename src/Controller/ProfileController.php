9d86b96ef0705141f9d7950d52c236d28d435e34
Violeta Doncheva <violeta.doncheva90@gmail.com>
2023-01-30 10:33:08 +0100

    start on edit user profile

--- src/Controller/ProfileController.php
+++ src/Controller/ProfileController.php
@@ -2,6 +2,11 @@
 
 namespace App\Controller;
 
+use App\Entity\User;
+use App\Form\ProfileType;
+use App\Form\UserType;
+use App\Repository\UserRepository;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -12,5 +17,27 @@ class ProfileController extends AbstractController
     public function index(): Response
     {
         return $this->render('profile/profile.html.twig');
+    }
+
+    #[Route('/editer', name: 'app_edit', methods: ['GET', 'POST'])]
+    public function editUser(
+        Request $request,
+        UserRepository $userRepository
+    ): Response {
+        $user = new User();
+        $form = $this->createForm(ProfileType::class, $user);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $userRepository->save($user, true);
+            $this->addFlash('success', 'La modification a été faite avec succès.');
+
+            return $this->redirectToRoute('app_profile', [], Response::HTTP_SEE_OTHER);
+        }
+
+        return $this->renderForm('app_profile/profile_form.html.twig', [
+            'user' => $user,
+            'form' => $form,
+        ]);
     }
 }
--- src/Form/ProfileType.php
+++ src/Form/ProfileType.php
@@ -4,23 +4,33 @@ namespace App\Form;
 
 use App\Entity\User;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Vich\UploaderBundle\Form\Type\VichImageType;
 
 class ProfileType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('email')
-            ->add('roles')
-            ->add('password')
-            ->add('firstName')
-            ->add('lastName')
-            ->add('profileImageName')
-            ->add('updatedAt')
-            ->add('lessons')
-        ;
+            ->add('firstName', TextType::class, [
+                'label' => 'Prénom',
+                'attr' => [
+                    'placeholder' => 'Jules'
+                ],
+            ])
+            ->add('lastName', TextType::class, [
+                'label' => 'Nom',
+                'attr' => [
+                    'placeholder' => 'Verne'
+                ],
+            ])
+            ->add('imageFile', VichImageType::class, [
+                'label' => 'Image',
+                'allow_delete'  => true,
+                'required'   => false,
+            ]);
     }
 
     public function configureOptions(OptionsResolver $resolver): void
