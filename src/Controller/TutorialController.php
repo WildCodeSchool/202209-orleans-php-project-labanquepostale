bfaa5b4b1d9aa36b52850118bc6e062ef8f977f2
SILVESTRI_GREGORY <gregory.silvestri45@gmail.com>
2023-02-06 10:13:34 +0100

    securityPages

--- src/Controller/TutorialController.php
+++ src/Controller/TutorialController.php
@@ -5,9 +5,10 @@ namespace App\Controller;
 use App\Entity\Tutorial;
 use App\Repository\LessonRepository;
 use App\Repository\TutorialRepository;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Security\Core\Exception\AccessDeniedException;
 
 #[Route('/tutoriel', name: 'tutorial_')]
 class TutorialController extends AbstractController
@@ -16,6 +17,10 @@ class TutorialController extends AbstractController
     public function index(TutorialRepository $tutorialRepository): Response
     {
         $tutorials = $tutorialRepository->findAll();
+        $user = $this->getUser();
+        if (!$user) {
+            throw new AccessDeniedException('Accès refusé');
+        }
 
         return $this->render('tutorial/index.html.twig', [
             'tutorials' => $tutorials,
@@ -26,6 +31,10 @@ class TutorialController extends AbstractController
     public function show(Tutorial $tutorial): Response
     {
         $lessons = $tutorial->getLessons();
+        $user = $this->getUser();
+        if (!$user) {
+            throw new AccessDeniedException('Accès refusé');
+        }
 
         return $this->render('lesson/index.html.twig', [
             'lessons' => $lessons,
