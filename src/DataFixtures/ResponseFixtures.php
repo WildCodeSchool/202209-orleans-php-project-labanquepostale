db81ef3865b56beef3466faf394f1ec201ff2a75
DRISS BOUMEDIENE <driss308@hotmail.fr>
2023-02-08 12:32:12 +0100

    fixtures réalistes

--- src/DataFixtures/ResponseFixtures.php
+++ src/DataFixtures/ResponseFixtures.php
@@ -32,30 +32,30 @@ class ResponseFixtures extends Fixture implements DependentFixtureInterface
     {
         $faker = Factory::create();
 
-        for ($tutoKey = 0; $tutoKey < count(TutorialFixtures::TUTORIALS); $tutoKey++) {
-            for ($lessonKey = 0; $lessonKey < count(LessonFixtures::LESSONS); $lessonKey++) {
-                for ($questionKey = 0; $questionKey < count(QuestionFixtures::QUESTIONS[$lessonKey]); $questionKey++) {
-                    if ($lessonKey == 2) {
-                        foreach (self::ANSWERS[$questionKey] as $key => $answerText) {
+        for ($i = 0; $i < count(TutorialFixtures::TUTORIALS); $i++) {
+            for ($j = 0; $j < count(LessonFixtures::LESSONS); $j++) {
+                for ($k = 0; $k < count(QuestionFixtures::QUESTIONS[$j]); $k++) {
+                    if ($j == 2) {
+                        foreach (self::ANSWERS[$k] as $key => $answerText) {
                             $response = new Explanation();
                             $response->setIsCorrect(false);
                             $response->setAnswer($answerText);
                             if ($key == 1) {
                                 $response->setIsCorrect(true);
                             }
-                            $question = $this->getReference('tutorial_' . $tutoKey . '_lesson_' . $lessonKey . '_question_' . $questionKey);
+                            $question = $this->getReference('tutorial_' . $i . '_lesson_' . $j . '_question_' . $k);
                             $response->setQuestion($question);
                             $manager->persist($response);
                         }
                     } else {
-                        for ($k = 0; $k < count(self::ANSWERS[$questionKey]); $k++) {
+                        for ($k = 0; $k < count(self::ANSWERS[$k]); $k++) {
                             $response = new Explanation();
                             $response->setAnswer($faker->sentence(3, true));
                             $response->setIsCorrect(false);
                             if ($k == 1) {
                                 $response->setIsCorrect(true);
                             }
-                            $question = $this->getReference('tutorial_' . $tutoKey . '_lesson_' . $lessonKey . '_question_' . $questionKey);
+                            $question = $this->getReference('tutorial_' . $i . '_lesson_' . $j . '_question_' . $k);
                             $response->setQuestion($question);
                             $manager->persist($response);
                         }
